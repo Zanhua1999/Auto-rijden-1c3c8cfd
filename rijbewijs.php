8b96252b0a5c33e2849e18874dc5aacc9c61828f
Zanhua1999 <skyair886@gmail.com>
2019-11-18 15:52:26 +0100

    Uitwerking van de exercise

---
+++ rijbewijs.php
@@ -0,0 +1,14 @@
+<?php
+$a = 16.5;
+
+echo "Hoe oud ben je?";
+echo "\n";
+
+$line = readline();
+
+if ($line <= $a) {
+echo "Helaas, je mag nog niet beginnen met rijlessen";
+} else if ($line > $a){
+echo "Je mag beginnen met rijlessen!";
+}
+?>
